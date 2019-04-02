101597a48ab4e7d228cc720258306f35f4208bea
Aamir Kalimi <aamirkalimi.edu@gmail.com>
2019-04-02 12:39:34 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,12 @@
 # Add  code here!
+require 'benchmark'
+def prime?(num)
+  if num < 0 || num == 1 || num ==0
+    false
+  else
+    (2..num-1).to_a.none? do |factor|
+      num % factor == 0
+    end
+  end
+end
+puts Benchmark.measure { prime?(16464000) }
